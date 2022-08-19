10f1a194a7275f718ba66da611216269cf40cf48
Andrew Ezzat <andrewezzat@Andrews-MacBook-Pro.local>
2022-08-20 01:26:43 +0200

    feat : courses tabs

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const searchText = document.querySelector(".search-place");
 const searchButton = document.querySelector(".search-button");
 let filterKey = "";
 
+
 const saveToLocalStorage = () => {
     localStorage.setItem('filterKey', filterKey);
 }
@@ -22,12 +23,13 @@ async function showCourses() {
     for(let i = 0 ; i < data.length ; i++){
         const courseIMG = data[i].image;
         const title = data[i].title;
-        const autor = data[i].title;
+        const autor = data[i].name;
         const rating = data[i].rating;
         const price = data[i].price;
+        const cID = data[i].cID;
         const course =
         `
-            <div class="course-card">
+            <div class="course-card tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
             <img src="${courseIMG}" alt="course thumbnail" class="course-img">
             <h3 class="course-title">${title}</h3>
             <p class="course-author">${autor}</p>
@@ -36,12 +38,13 @@ async function showCourses() {
                 <img class = "rating-stars" src="https://www.pngplay.com/wp-content/uploads/6/5-Star-Rating-Vector-Transparent-PNG.png" alt="rating">
             </div>
             <h3 class="course-price">${price}</h3>
-                </div>
+            </div>
         `
         let titleLC = title.toLowerCase();
         let filterLC = localStorage.getItem("filterKey");
         if(titleLC.includes(filterLC)){
-            document.querySelector(".topic-courses").innerHTML += course;
+            document.querySelector(cID).innerHTML += course;
+
         }
         
     }
